3d40d7cb776c4743e3bfd2ddb6f3443227c250b2
Bui Hue Khanh <khanhc2200005@studen.ctu.edu.vn>
2024-10-31 03:49:26 +0700

    Cai dat cac handle truy xuat CSDL

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -1,8 +1,13 @@
 const { MongoClient } = require("mongodb");
 
 class MongoDB {
+    static client = null;
+
     static connect = async (uri) => {
         if (this.client) return this.client;
         this.client = await MongoClient.connect(uri);
+        return this.client; // Trả về client sau khi kết nối
     };
 }
+
+module.exports = MongoDB;
